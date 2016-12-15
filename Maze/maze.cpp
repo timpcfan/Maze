cb2c29e92c9e2e1e9721ace3597d710ba6c5578f
TrystanLei <lztsmail@gmail.com>
2016-12-15 23:07:49 +0800

    添加项目文件。

---
+++ Maze/maze.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include "MyMazeMap.h"
+#include "MyMazer.h"
+
+using namespace std;
+
+const int SUCCESS = 0;
+int main()
+{
+	int map[8][9] = {
+					{WALL,WALL,WALL,WALL,WALL,WALL,WALL,ROAD,WALL},
+					{WALL,WALL,WALL,WALL,ROAD,WALL,WALL,ROAD,WALL},
+					{WALL,WALL,WALL,WALL,ROAD,WALL,WALL,ROAD,WALL},
+					{WALL,WALL,WALL,WALL,ROAD,WALL,WALL,ROAD,WALL},
+					{WALL,WALL,ROAD,ROAD,ROAD,ROAD,WALL,ROAD,WALL},
+					{WALL,WALL,ROAD,WALL,WALL,ROAD,ROAD,ROAD,WALL},
+					{WALL,ROAD,ROAD,WALL,WALL,WALL,WALL,WALL,WALL},
+					{WALL,ROAD,WALL,WALL,WALL,WALL,WALL,WALL,WALL}
+					};
+	MyMazeMap maze;
+	
+	//maze.defaultMode();
+	maze.setMazeMap(&map[0][0],8,9);
+	maze.setMazeWall('*');
+	maze.drawMap();
+	MyMazer mazer;
+	mazer.setPersonPosition(1,7);
+	mazer.setPersonSpeed(FAST);
+	mazer.setPersonChar('T');
+	mazer.start();
+	
+	system("pause");
+	return SUCCESS;
+}
